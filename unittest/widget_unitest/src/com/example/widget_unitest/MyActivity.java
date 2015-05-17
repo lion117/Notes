fd0b58369c731d735021a79f6515514210f46194
lion_117 <lion_117@126.com>
2015-05-17 17:37:22 +0800

    add image for button

--- unittest/widget_unitest/src/com/example/widget_unitest/MyActivity.java
+++ unittest/widget_unitest/src/com/example/widget_unitest/MyActivity.java
@@ -1,34 +1,45 @@
 package com.example.widget_unitest;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
 
-    final  Button btn_img;
+//    final  Button btn_img;
+    ImageButton imag_btn;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        btn_img = (Button)findViewById(R.id.btn_img);
-        btn_img.setOnClickListener(new View.OnClickListener() {
+        imag_btn = (ImageButton)findViewById(R.id.img_btn);
+        imag_btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                imag_btn.setImageDrawable(Drawable.createFromPath("E:/Git_open_project/Notes/unittest/widget_unitest/res/drawable-xhdpi/new_note_pressed.png"));
+                }
+                else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    imag_btn.setImageDrawable(Drawable.createFromPath("E:/Git_open_project/Notes/unittest/widget_unitest/res/drawable-xhdpi/new_note_normal.png"));
+                }
 
-                
+                return  false;
             }
         });
-
-
-
-
     }
+
+
+
+
 
 
 
